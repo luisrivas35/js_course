1cc69aea272c0748b400a772d2e04370ad92e061
Luis Rivas <xluisrivas@gmail.com>
2024-02-27 20:44:26 -0300

    update

--- desafios/cachipun/assets/js/cachipun_main.js
+++ desafios/cachipun/assets/js/cachipun_main.js
@@ -3,27 +3,33 @@ document.getElementById("comenzar").addEventListener("click", function () {
 });
 
 function jugar() {
-  const jugadasCPU = ["piedra", "papel", "tijera"];
-
+  const jugadas = ["piedra", "papel", "tijera"];
   const jugadaUsuario = prompt("Elige tu jugada (Piedra, Papel, Tijera):")
     .trim()
     .toLowerCase();
 
-  const jugadaCPU = jugadasCPU[Math.floor(Math.random() * 3)];
-  let resultado = "";
-  if (
-    (jugadaUsuario === "piedra" && jugadaCPU === "tijera") ||
-    (jugadaUsuario === "papel" && jugadaCPU === "piedra") ||
-    (jugadaUsuario === "tijera" && jugadaCPU === "papel")
-  ) {
-    resultado = "¡Ganaste!";
-  } else if (jugadaUsuario === jugadaCPU) {
-    resultado = "¡Es un empate!";
-  } else {
-    resultado = "¡Perdiste!";
-  }
+  if (jugadaUsuario && jugadas.includes(jugadaUsuario)) {
+    // opción válida
+      const jugadaCPU = jugadas[Math.floor(Math.random() * 3)];
+      let resultado = "";
+      if (
+        (jugadaUsuario === "piedra" && jugadaCPU === "tijera") ||
+        (jugadaUsuario === "papel" && jugadaCPU === "piedra") ||
+        (jugadaUsuario === "tijera" && jugadaCPU === "papel")
+      ) {
+        resultado = "¡Ganaste!";
+      } else if (jugadaUsuario === jugadaCPU) {
+        resultado = "¡Es un empate!";
+      } else {
+        resultado = "¡Perdiste!";
+      }
 
-  document.getElementById(
-    "resultado"
-  ).innerHTML += `Tu jugada fue: ${jugadaUsuario}, Jugada de la máquina: ${jugadaCPU}. El resultado: ${resultado}</p>`;
+      document.getElementById(
+        "resultado"
+      ).innerHTML += `Tu jugada fue: ${jugadaUsuario}, Jugada de la máquina: ${jugadaCPU}. El resultado: ${resultado}</p>`;
+      
+
+  }else {
+    alert("Por favor, ingresa una opción válida (Piedra, Papel, Tijera)!");
+  }
 }
