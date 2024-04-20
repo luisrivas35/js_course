59088bafb4d66f1bfeec712e3ec173a5f760add4
Luis Rivas <xluisrivas@gmail.com>
2024-04-20 13:05:03 -0400

    update changes

--- desafios/services/index.js
+++ desafios/services/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { engine } from 'express-handlebars';
 import path from 'path'
-
+import { services } from './data/services.data.js'
 
 const app = express()
 
@@ -17,49 +17,6 @@ app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', __dirname + '/views');
 
-export const services = [
-  {
-    name: "Web Development",
-    description: "We create web applications with the latest technologies.",
-    url: "/services/web-development",
-  },
-  {
-    name: "Mobile Development",
-    description: "We create mobile applications with the latest technologies.",
-    url: "/services/mobile-development",
-  },
-  {
-    name: "DevOps",
-    description: "We create CI/CD pipelines with the latest technologies.",
-    url: "/services/devops",
-  },
-  {
-    name: "QA",
-    description: "We create test cases with the latest technologies.",
-    url: "/services/qa",
-  },
-  {
-    name: "UX/UI",
-    description: "We create designs with the latest technologies.",
-    url: "/services/ux-ui",
-  },
-  {
-    name: "Training",
-    description: "We create training with the latest technologies.",
-    url: "/services/training",
-  },
-  {
-    name: "Consulting",
-    description: "We create consulting with the latest technologies.",
-    url: "/services/consulting",
-  },
-  {
-    name: "Outsourcing",
-    description: "We create outsourcing with the latest technologies.",
-    url: "/services/outsourcing",
-  },
-];
-
 
 app.get('/', (req, res) => {
     res.render('')
@@ -70,28 +27,33 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/services', (req, res) => {
-    res.render('services', { services: services })
+    res.render('services', { services });
 })
 
 app.get("/services/:path", (req, res) => {
   try {
     const path = req.params.path;
+    
     const service = services.find(
       (service) => service.url === `/services/${path}`
     );
-
+    
     if (!service) {
-     
-      return res.render("error-page");
+      return res.render("error-page", { title: "Service Not Found" });
     }
-  
-    return res.render(`services/${path}`);
+   
+    return res.render('des-service', { service });
+
   } catch (error) {
-  
-    return res.render("error-page");
+   
+    return res.render("error-page", { title: "Some error come out !!!" });
   }
 });
 
+
+app.get('*', (req, res) => {
+    res.status(404).render("error-page", { title: "Page Not Found" });
+});
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
