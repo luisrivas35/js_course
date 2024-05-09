3eceda9705defa49a3c12965f74cef385a15e0f8
Luis Rivas <xluisrivas@gmail.com>
2024-05-08 23:02:16 -0400

    update

--- desafios/always-music/database/queries.sql
+++ desafios/always-music/database/queries.sql
@@ -7,3 +7,9 @@ CREATE TABLE IF NOT EXISTS students (
     Curso VARCHAR(50),
     Nivel VARCHAR(50)
 );
+
+INSERT INTO students (rut, nombre, curso, nivel) VALUES
+    ('27.681.725-6', 'Pablito Egaña', 'historia', 'medio'),
+    ('87.399.494-0', 'Karol Ina', 'contabilidad', 'avanzado'),
+    ('44.894.919-2', 'Carlos Pacheco', 'derecho', 'avanzado'),
+    ('49.992.148-9', 'Carolina Perez', 'biologia', 'avanzado');
